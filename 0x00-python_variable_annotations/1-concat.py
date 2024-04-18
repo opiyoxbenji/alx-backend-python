2add1b768b9aa8b31e245d5c9ec1801eda82f09a
opiyoxbenji <opiyosbenji@gmail.com>
2024-04-18 12:49:35 +0000

    new file

---
+++ 0x00-python_variable_annotations/1-concat.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python3
+"""
+Concatenation function
+"""
+
+def concat(str1, str, str2: str) -> str:
+    """
+    Concatenates two strings and returns the result
+    """
+    return str1 + str2
