ea919ab804c869084a1838ca7242c2c3c82deb90
opiyoxbenji <opiyosbenji@gmail.com>
2024-04-18 12:58:42 +0000

    returns a tring repr_n of a float

---
+++ 0x00-python_variable_annotations/3-to_str.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python3
+"""
+String representation of a float
+"""
+
+
+def to_str(n: float) -> str:
+    """
+    Converts a float ot a string represenation
+    """
+    return str(n)
