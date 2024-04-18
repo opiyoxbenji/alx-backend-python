1cb7dd6ea1f22463120f8388c1716f40a4d9e858
opiyoxbenji <opiyosbenji@gmail.com>
2024-04-18 13:05:42 +0000

    gets the sum off floats

---
+++ 0x00-python_variable_annotations/5-sum_list.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python3
+"""
+Computes the sum of floats
+"""
+from typing import List
+
+
+def sum_list(input_list: List[float]) -> float:
+    """
+    Sum of floats
+    """
+    return sum(input_list)
