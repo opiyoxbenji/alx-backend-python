c53e32425756c55cd2163e4ac013f947878214b3
opiyoxbenji <opiyosbenji@gmail.com>
2024-04-18 13:09:25 +0000

    Computes the sum of a list containing integers and floats

---
+++ 0x00-python_variable_annotations/6-sum_mixed_list.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python3
+"""
+Computes the sum of lists with integers and floats
+"""
+from typing import List, Union
+
+
+def sum_mixed_list(mxd_list: List[Union[int, float]]) -> float:
+    """
+    Sum of a list with floats and integers
+    """
+    return sum(mxs_list)
