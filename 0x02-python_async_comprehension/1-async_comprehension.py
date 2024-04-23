8370ee676460794470c21ec11cae20bae9a5372c
opiyoxbenji <opiyosbenji@gmail.com>
2024-04-23 12:20:50 +0000

    Creates a list of 10 numbers from a 10-number generator

--- 0x02-python_async_comprehension/1-async_comprehension.py
+++ 0x02-python_async_comprehension/1-async_comprehension.py
@@ -1,15 +1,18 @@
 #!/usr/bin/env python3
-'''Task 1 - async comprehensions
-'''
+"""
+Task 1 - async comprehensions
+"""
 
-import typing
+from typing import List
+from importlib import import_ module as using
 
-async_generator = __import__('0-async_generator').async_generator
+
+async_generator = using('0-async_generator').async_generator
 
 
-async def async_comprehension() -> typing.List[float]:
+async def async_comprehension() -> List[float]:
     """
     Collects 10 random numbers using an async comprehensing over async_generator
     then return the 10 random numbers
     """
-    return [rand async for rand in async_generator()]
+    return [num async for num in async_generator()]
